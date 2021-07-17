85aef756e4a96bef121caf4c04fd94b11bf8cc0d
Heron Peixoto <heronbpeixoto@gmail.com>
2021-07-17 15:24:10 -0300

    .

--- ErrorProviderExample/Form1.Designer.cs
+++ ErrorProviderExample/Form1.Designer.cs
@@ -1,14 +1,15 @@
-﻿namespace ErrorProviderExample
+﻿
+namespace ErrorProviderExample
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -17,24 +18,98 @@
             {
                 components.Dispose();
             }
-
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User:";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(166, 37);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(152, 23);
+            this.tbUser.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(166, 95);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(152, 23);
+            this.tbPassword.TabIndex = 2;
+            // 
+            // btLogin
+            // 
+            this.btLogin.Location = new System.Drawing.Point(219, 166);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(99, 23);
+            this.btLogin.TabIndex = 3;
+            this.btLogin.Text = "Login";
+            this.btLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(367, 213);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ErrorProviderExample/Form1.cs
+++ ErrorProviderExample/Form1.cs
@@ -4,17 +4,121 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DynamicData;
+using DynamicData.Binding;
+using ReactiveUI;
+using ReactiveUI.Validation.Components.Abstractions;
+using ReactiveUI.Validation.States;
 
 namespace ErrorProviderExample
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IViewFor<Form1ViewModel>
     {
-        public Form1()
+        public Form1(Form1ViewModel viewModel)
         {
+            this.ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+            
             InitializeComponent();
+            
+            this.WhenActivated(
+                disposables =>
+                {
+                    this
+                        .Bind(this.ViewModel, vm => vm.User, v => v.tbUser.Text)
+                        .DisposeWith(disposables);
+                    
+                    this
+                        .Bind(this.ViewModel, vm => vm.Password, v => v.tbPassword.Text)
+                        .DisposeWith(disposables);
+                    
+                    this
+                        .BindCommand(this.ViewModel, vm => vm.Login, v => v.btLogin)
+                        .DisposeWith(disposables);
+                    
+                    this
+                        .ViewModel?
+                        .Logon
+                        .RegisterHandler(
+                            context =>
+                            {
+                                MessageBox.Show("Logged In!");
+                                context.SetOutput(Unit.Default);
+                            })
+                        .DisposeWith(disposables);
+                    
+                    //My best take at getting the list of validation errors out of the ValidationContext, so far.
+                    //It seems messy, but I took the idea directly from ReactiveValidationObject's ctor though...
+                    //This is probably the source of my problems, see below, so suggestions welcome!
+                    var propVals = 
+                            this
+                                .ViewModel?
+                                .ValidationContext
+                                .Validations
+                                .ToObservableChangeSet()
+                                .ToCollection()
+                                .Select(
+                                    comps =>
+                                        comps
+                                            .Select(
+                                                comp => 
+                                                    comp
+                                                        .ValidationStatusChange
+                                                        .Select(_ => comp))
+                                            .Merge() //Merge instead of Switch, as in the reference ctor.  
+                                            .StartWith(this.ViewModel.ValidationContext)
+                                )
+                                .Merge()
+                                .Where(comp => comp is IPropertyValidationComponent)
+                                .Cast<IPropertyValidationComponent>();
+                    
+                    //The idea is to use the list of errors to call the error provider set error function when appropriate.
+                    //However, the fields are starting already marked as error, and in the case of password, only the last
+                    //one is being shown. How can I prevent the observable to start in error, and how does one get all errors
+                    //associated with a single property?
+                    //Also, it does not seem to be computing both validations for password, as in I can input only 2 characters
+                    //without triggering either the SetError on the provider or IsValid=False on the context. How come? 
+                    propVals?
+                        .Where(x => x.ContainsPropertyName(nameof(this.ViewModel.User)))
+                        .Do(
+                            x => 
+                                this.errorProvider.SetError(this.tbUser, x.Text?.ToSingleLine("\n")))
+                        .Subscribe()
+                        .DisposeWith(disposables);
+                    
+                    propVals?
+                        .Where(x => x.ContainsPropertyName(nameof(this.ViewModel.Password)))
+                        .Do(
+                            x =>
+                                //When multiple errors happen, isn't this supposed to return all error messages?
+                                this.errorProvider.SetError(this.tbPassword, x.Text?.ToSingleLine("\n")))
+                        .Subscribe()
+                        .DisposeWith(disposables);
+                }
+            );
         }
+
+        #region Implementation of IViewFor
+
+        /// <inheritdoc />
+        object? IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (Form1ViewModel)value;
+        }
+
+        #endregion
+
+        #region Implementation of IViewFor<Form1ViewModel>
+
+        /// <inheritdoc />
+        public Form1ViewModel? ViewModel { get; set; }
+
+        #endregion
     }
 }
